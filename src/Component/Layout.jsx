b25ec7c6082f3465f835b39cd45f0f04cc48cb38
mohamed esmail <abohadida51@gmail.com>
2024-09-11 01:07:54 +0300

    7

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 
 export default function Layout() {
   return (
-    <div className='parent'>
+    <div className='parent '>
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
